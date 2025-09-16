0f2651ec3922fd7002fbf4247bbf345f87695015
linux-dev <160027919+linux-dev-arch@users.noreply.github.com>
2025-09-16 14:06:35 +0530

    Update cherryscript.py

--- cherryscript.py
+++ cherryscript.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import linecache ,sys
-var=[]#list to store variables
-val=[]#list to store variable values
+intvar=[]#list to store variables
+intval=[]#list to store variable values
+strvar=[]
+strval=[]
 x = 1
 File = sys.argv[1]
 def tokeniser(file,line):
@@ -15,7 +17,14 @@ def compute(tokens):
                 pass#do nothing :)
         elif tokens[0] == "print":
                 for i in range(1,len(tokens)):
-                        print(tokens[i],end=" ")
+                        if tokens[i] in strvar:
+                                indx = strvar.index(tokens[i])
+                                print(strval[indx],end="")
+                        elif tokens[i] in intvar:
+                                indx = intvar.index(tokens[i])
+                                print(intval[indx],end="")
+                        else:
+                                print(tokens[i],end=" ")
                 print()
         elif tokens[0] == "end":
                 exit()
@@ -29,44 +38,90 @@ def compute(tokens):
                         compute(a)
                         count+= 1
         elif tokens[0] == "add":
-                if 3<len(tokens):
-                            print("Error:Can only add two numbers!!!!")
-                            exit(1)
+                if len(tokens) > 3:
+                        if tokens[3] in intvar:
+                                temp=eval(tokens[1])+eval(tokens[2])
+                                print(temp)
+                                indx = intvar.index(tokens[3])
+                                intval[indx]=temp
+                        else:
+                                print("ERROR:UNKNOW VARIABLE!")
                 else:
                         temp=eval(tokens[1])+eval(tokens[2])
                         print(temp)
         elif tokens[0] == "diff":
-                if 3<len(tokens):
-                        print("Error:Can only subtract two numbers!!!")
+                if len(tokens) > 3:
+                        if tokens[3] in intvar:
+                                temp=eval(tokens[1])-eval(tokens[2])
+                                print(temp)
+                                indx = intvar.index(tokens[3])
+                                intval[indx]=temp
+                        else:
+                                print("ERROR:UNKNOW VARIABLE!")
                 else:
-                        temp = eval(tokens[1]) - eval(tokens[2])
+                        temp=eval(tokens[1])-eval(tokens[2])
                         print(temp)
         elif tokens[0] == "div":
-                if 3<len(tokens):
-                        print("Error:Only TWO numbers can be subtracted!!!!")
+                if len(tokens) > 3:
+                        if tokens[3] in intvar:
+                                temp=eval(tokens[1])/eval(tokens[2])
+                                print(temp)
+                                indx = intvar.index(tokens[3])
+                                intval[indx]=temp
+                        else:
+                                print("ERROR:UNKNOW VARIABLE!")
                 else:
-                        temp = eval(tokens[1])/eval(tokens[2])
+                        temp=eval(tokens[1])/eval(tokens[2])
                         print(temp)
         elif tokens[0] =="mul":
-                temp = eval(tokens[1])
-                for i in range(2,len(tokens)):
-                        temp = temp * eval(tokens[i])
-                print(temp)
-        elif tokens[0] == "var":
-                if tokens[1].startswith("$"):
-                        var.append(tokens[1])
-                        if len(tokens) == 3:
-                                val.append(tokens[2])#assign a value if there is a value after the variable name
+                if len(tokens) > 3:
+                        if tokens[3] in intvar:
+                                temp=eval(tokens[1])*eval(tokens[2])
+                                print(temp)
+                                indx = intvar.index(tokens[3])
+                                intval[indx]=temp
                         else:
-                                val.append(0)
+                                print("ERROR:UNKNOW VARIABLE!")
+                else:
+                        temp=eval(tokens[1])*eval(tokens[2])
+                        print(temp)
+        elif tokens[0] == "var":
+                if tokens[2].startswith("$"):
+                        if tokens[1] == "int":
+                                intvar.append(tokens[2])
+                                if len(tokens)==4:
+                                        intval.append(tokens[3])
+                                else:
+                                        intval.append(0)
+                        elif tokens[1] == "str":
+                                strvar.append(tokens[2])
+                                if len(tokens)==4:
+                                        strval.append(tokens[3])
+                                else:
+                                        strval.append('')
+                        else:
+                                print("Error:Unkown data type please check your code :(")
+
                 else:
                         print("Error:variable should start with '$' !!!!!")
         elif tokens[0] == "assign":
-                if tokens[1] in var:
-                        indx=var.index(tokens[1])
-                        val[indx]=tokens[2]
+                if tokens[1] in intvar:
+                        indx=intvar.index(tokens[1])
+                        intval[indx]=eval(tokens[2])
+                elif tokens[1] in strvar:
+                        indx=strvar.index(tokens[1])
+                        for i in range(2,len(tokens)):
+                                strval[indx]+=tokens[i]
+                else:
+                        print("Error:Variable not found!!!!")
+        elif tokens[0] == "inc":
+                if tokens[1] in intvar:
+                        indx=intvar.index(tokens[1])
+                        intval[indx]+=eval(tokens[2])
+                else:
+                        print("ERROR:Variable not found")
 while True:
         token = tokeniser(File,x)
         compute(token)
         x +=1
-        #print(var,val,sep="\n") just for debugging variable implementation :)
+        #print(intvar,intval,sep="\n") #just for debugging variable implementation :)
